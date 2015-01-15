1944e2e5a02338d8cfb8da85dd237cf6844827a8
Vonkristoff <mrjcnicolas@gmail.com>
2015-01-15 10:29:47 +0000

    adding basic render loop and scroll events

--- module.js
+++ module.js
@@ -3,6 +3,7 @@ var Paxman = function(){
   var query = Array.prototype.slice.call(document.querySelectorAll("section"));
 
   this.scope = {
+    scroll:{},
     sections:{},
     current:0
   }
@@ -17,7 +18,13 @@ px.init = function(query){
 
   // build the model
   this.establishModel(query);
-  console.log(this.scope.sections);
+
+  this.addScrollEvents();
+  
+  // start render engine
+  // requestAnimationFrame(function(){
+  //   this.enterFrame()
+  // });
 }
 
 px.establishModel = function(q){
@@ -72,13 +79,113 @@ px.establishModel = function(q){
       children: getChildren(el)
     }
   }.bind(this))
+
+  this.scope.total = Object.keys(this.scope.sections).length;
+}
+
+px.visibility = function(j){
+
+  var scroll_bottom = window.pageYOffset + window.innerHeight,
+      scroll_top = window.pageYOffset,
+      bottomThreshold =  scroll_bottom - sections[j].top;
+
+  function cloak(who, bool){
+    if(bool){
+      if(who.visible) {
+        this.css(el,{'visibility':'hidden'})
+        who.visible = false;
+      }       
+    } else {
+      if(!who.visible){
+        this.css(el,{'visibility':'visible'})
+        who.visible = true;
+      }
+    }
+  }
+
+  // does the threshold pass the bottom of the visible area
+  if(bottomThreshold > 0){
+    // now check if it has passed the top of the visible area
+    var topThreshold = bottomThreshold - window.innerHeight
+    if(topThreshold > sections[j].height){
+      // you aint visible
+      cloak(sections[j],true)
+    } else {
+      // you are visible
+      cloak(sections[j],false)
+    }
+  } else {
+    cloak(sections[j],true)
+  }
+
 }
 
 
-
+px.currentAnchor = function(){
+  // tests the scroll positions of elements and returns the element of main focus
+  var s = this.scope,
+      scroll = s.scroll.scroll_top;
+  for(var i=0;i<s.total;i++){
+    if(scroll > 0){
+      if(scroll > s.sections[i].max){
+        // try next
+      } else if(scroll > s.sections[i].top){
+        return i;
+      }
+    } else {
+      return 0
+    }
+  }
+}
+// use method to add css properties to element 
+px.css = function(target, properties) {
+  for(var key in properties) {
+    target.style[ key ] = properties[ key ];
+  }
+}
+// use to extend lib in main script
 px.api = function(){
 
 }
+
+px.render = function(){
+
+}
+
+px.evaluate = function(i){
+
+}
+
+px.addScrollEvents = function(){
+  
+  window.addEventListener('scroll', function (e){
+    
+    var w = window,
+        s = this.scope.scroll;
+    
+    s.scroll_top     = w.pageYOffset; // default scroll position
+    s.view_height    = w.innerHeight; // browser height
+    s.scroll_bottom  = s.scroll_top + s.view_height; // scroll pos at bottom of view
+
+    for(var i=0;i<this.scope.total;i++){
+      this.evaluate(i);
+    }
+
+    this.scope.current = this.currentAnchor();
+
+  }.bind(this))
+}
+
+px.enterFrame = function(){
+
+  this.render()
+
+  requestAnimationFrame(function(){
+    this.enterFrame()
+  });
+}
+
+
 
 String.prototype.contains = function(test) {
   return this.indexOf(test) == -1 ? false : true;
